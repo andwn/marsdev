def363ca0660e86b82fc5bf766a9c04704c70b46
Andrew DeRosier <andy@skychase.zone>
2019-12-25 01:19:09 -0500

    Add moon to SGDK sample to make sure rescomp is working

--- examples/sgdk-skeleton/src/main.c
+++ examples/sgdk-skeleton/src/main.c
@@ -1,8 +1,12 @@
 #include <genesis.h>
+#include "resources.h"
 
 int main() {
-	PAL_setColor(0, 0x080);
 	VDP_drawText("Hello World!", 4, 4);
+	// Fromt he moon example
+	VDP_setPalette(PAL1, moon.palette->data);
+	VDP_drawImageEx(PLAN_A, &moon, TILE_ATTR_FULL(PAL1, 0, 0, 0, 1), 12, 12, 0, CPU);
+	
 	while(TRUE) {
 		VDP_waitVSync();
 	}
