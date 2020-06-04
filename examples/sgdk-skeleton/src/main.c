b66930a96eefd0bd3c34c0bf5ae2efa71a5e6183
Andrew DeRosier <andy@skychase.zone>
2020-06-04 12:42:33 -0400

    Fixes #2 -- updated sgdk example and ld script

--- examples/sgdk-skeleton/src/main.c
+++ examples/sgdk-skeleton/src/main.c
@@ -5,7 +5,7 @@ int main() {
 	VDP_drawText("Hello World!", 4, 4);
 	// Fromt he moon example
 	VDP_setPalette(PAL1, moon.palette->data);
-	VDP_drawImageEx(PLAN_A, &moon, TILE_ATTR_FULL(PAL1, 0, 0, 0, 1), 12, 12, 0, CPU);
+	VDP_drawImageEx(VDP_PLAN_A, &moon, TILE_ATTR_FULL(PAL1, 0, 0, 0, 1), 12, 12, 0, CPU);
 	
 	while(TRUE) {
 		VDP_waitVSync();
